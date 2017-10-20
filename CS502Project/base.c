d8e66e68bd088007e00ee3d3b331439a86ec59ff
qwang12 <qwang12@wpi.edu>
2017-10-20 02:32:28 -0400

    submit

--- CS502Project/base.c
+++ CS502Project/base.c
@@ -526,8 +526,8 @@ void osInit(int argc, char *argv[]) {
 	long ErrorReturned = 0;
 	//default test is test0, change test here for testing
 	if (argv[1] == NULL) {
-		testAddress = (long)test13;
-		testName = "test13";
+		testAddress = (long)test0;
+		testName = "test0";
 		printFullScheduler = 0;
 	}
 	else if (strcmp(argv[1], "test1") == 0) {
